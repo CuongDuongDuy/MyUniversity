4b2093902d5ca56a26e309f123c7958aecbb3363
CuongDuongDuy <cuong.duongduy@gmail.com>
2015-12-21 03:03:10 +0000

    #Commit missing files.

---
+++ MyUniversity.Contracts/Helpers/ValueHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MyUniversity.Contracts.Helpers
+{
+    public static class ValueHelper
+    {
+        public static string RemoveExtraSpace(this string source)
+        {
+            return Regex.Replace(source, @"\s+", " ");
+        }
+
+        public static string ByteArrayToString(this byte[] ba)
+        {
+            var hex = new StringBuilder(ba.Count() * 2);
+            foreach (var b in ba)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+
+            return hex.ToString();
+        }
+
+        public static byte[] StringToByteArray(this string hexString)
+        {
+            int length = hexString.Length;
+            int upperBound = length / 2;
+            if (length % 2 == 0)
+            {
+                upperBound -= 1;
+            }
+            else
+            {
+                hexString = "0" + hexString;
+            }
+            byte[] bytes = new byte[upperBound + 1];
+            for (int i = 0; i <= upperBound; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+}
---
+++ MyUniversity.Web/Controllers/DepartmentsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MyUniversity.Contracts.Models;
+using MyUniversity.Contracts.Services;
+using MyUniversity.Contracts.ViewModels;
+using MyUniversity.Contracts.Helpers;
+
+namespace MyUniversity.Web.Controllers
+{
+    public class DepartmentsController : BaseController
+    {
+
+        public async Task<ViewResult> Index()
+        {
+            var getDeanTask = GetHttpResponMessageResultAsyc<List<DepartmentModel>>("api/departments", "Dean",
+                "Dean.Person");
+            var viewModel = new DepartmentIndexViewModel
+            {
+                Departments = await getDeanTask
+            };
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<ViewResult> Create()
+        {
+            var getDeanTask = GetHttpResponMessageResultAsyc<List<TeacherModel>>("api/teachers");
+
+            var deans = await getDeanTask;
+            ViewBag.DeanId = new SelectList(deans, "Id", "FullName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(DepartmentModel departmentModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("BadRequest", "Error");
+            }
+            var guid = await PostJsonAsyc("api/departments", departmentModel);
+            return RedirectToAction("Details", "Departments", new {id = guid});
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var requestUri = string.Format("api/departments/{0}", id);
+            var departmentModel =
+                await GetHttpResponMessageResultAsyc<DepartmentModel>(requestUri, "Dean", "Dean.Person");
+            if (departmentModel == null) return HttpNotFound();
+            return View(departmentModel);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(Guid id, bool? concurrencyError)
+        {
+            var requestUri = string.Format("api/departments/{0}", id);
+            var departmentModel = await GetHttpResponMessageResultAsyc<DepartmentModel>(requestUri);
+            if (departmentModel == null) return HttpNotFound();
+            var teachersModels = await GetHttpResponMessageResultAsyc<List<TeacherModel>>("api/teachers");
+            ViewBag.DeanId = new SelectList(teachersModels, "Id", "FullName", departmentModel.DeanId);
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ViewBag.ConcurrencyErrorMessage = "Concurrency issue. If you still want to deactivate this "
+                                                  + "record, click the Save button again. Otherwise "
+                                                  + "click the Back to List hyperlink.";
+            }
+            return View(departmentModel);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(DepartmentModel departmentModel)
+        {
+            var requestUri = string.Format("api/departments/{0}", departmentModel.Id);
+            if (!ModelState.IsValid) throw new HttpException((int) HttpStatusCode.BadRequest, "Error");
+            var updated = await PutJsonAsyc(requestUri, departmentModel);
+            switch (updated.Type)
+            {
+                case ResultType.DbUpdateConcurrencyException:
+                    return RedirectToAction("Edit", new { id = departmentModel.Id, concurrencyError = true });
+                case ResultType.DataException:
+                    ModelState.AddModelError(string.Empty,
+                        "Unable to deactivate. Try again, and if the problem persists contact your system administrator.");
+                    return View(departmentModel);
+            }
+            return RedirectToAction("Details", "Departments", new {id = updated.Value});
+        }
+
+        [HttpGet, ActionName("Delete")]
+        public async Task<ActionResult> Deactivate(Guid id, bool? concurrencyError)
+        {
+            var requestUri = string.Format("api/departments/{0}", id);
+            var departmentModel =
+                await GetHttpResponMessageResultAsyc<DepartmentModel>(requestUri, "Dean", "Dean.Person");
+            if (departmentModel == null) return HttpNotFound();
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ViewBag.ConcurrencyErrorMessage = "Concurrency issue. If you still want to deactivate this "
+                                                  + "record, click the Deactivate button again. Otherwise "
+                                                  + "click the Back to List hyperlink.";
+            }
+            return View(departmentModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Deactivate(DepartmentModel departmentModel)
+        {
+            if (departmentModel == null) return HttpNotFound();
+            var requestUri = string.Format("api/departments/{0}/deactivate?rowVersion={1}", departmentModel.Id, departmentModel.RowVersion.ByteArrayToString());
+            var deleted = await DeleteJsonAsyc(requestUri);
+            switch (deleted.Type)
+            {
+                case ResultType.DbUpdateConcurrencyException:
+                    return RedirectToAction("Delete", new {id = departmentModel.Id, concurrencyError = true});
+                case ResultType.DataException:
+                    ModelState.AddModelError(string.Empty,
+                        "Unable to deactivate. Try again, and if the problem persists contact your system administrator.");
+                    return View(departmentModel);
+            }
+            return RedirectToAction("Details", "Departments", new { id = departmentModel.Id });
+        }
+
+        [HttpGet, ActionName("Restore")]
+        public async Task<ActionResult> Reactivate(Guid id, bool? concurrencyError)
+        {
+            var requestUri = string.Format("api/departments/{0}", id);
+            var departmentModel =
+                await GetHttpResponMessageResultAsyc<DepartmentModel>(requestUri, "Dean", "Dean.Person");
+            if (departmentModel == null) return HttpNotFound();
+            if (concurrencyError.GetValueOrDefault())
+            {
+                ViewBag.ConcurrencyErrorMessage = "Concurrency issue. If you still want to reactivate this "
+                                                  + "record, click the Reactivate button again. Otherwise "
+                                                  + "click the Back to List hyperlink.";
+            }
+            return View(departmentModel);
+        }
+
+        [HttpPost, ActionName("Restore")]
+        public async Task<ActionResult> Reactivate(DepartmentModel deparmentModel)
+        {
+            var requestUri = string.Format("api/departments/{0}/activate?rowVersion={1}", deparmentModel.Id, deparmentModel.RowVersion.ByteArrayToString());
+            var restored = await DeleteJsonAsyc(requestUri);
+            switch (restored.Type)
+            {
+                case ResultType.DbUpdateConcurrencyException:
+                    return RedirectToAction("Restore", new { id = deparmentModel.Id, concurrencyError = true });
+                case ResultType.DataException:
+                    ModelState.AddModelError(string.Empty,
+                        "Unable to reactivate. Try again, and if the problem persists contact your system administrator.");
+                    return View(deparmentModel);
+            }
+            return RedirectToAction("Details","Departments", new{ id = deparmentModel.Id});
+        }
+    }
+}
